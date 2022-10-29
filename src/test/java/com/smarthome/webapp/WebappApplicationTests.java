d2b415a1b63b18b831d012bd4c267bbaf1d62343
jroux14 <jroux14@gmail.com>
2022-10-28 20:43:26 -0400

    Initial Commit

---
+++ src/test/java/com/smarthome/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthome.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
